868b3ead75cc703cea483b6cbcc2106d15c1cc3d
KCN007 <www.kaniz98@gmail.com>
2020-05-06 02:38:43 +0600

    excercise 5

---
+++ 5_image.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Typography</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- ROUND EDGE IMAGE -->
+  <img src="imagesbs/kaniz.jpg" class="img-rounded" width="250" height="250"> 
+<!-- ROUND EDGE IMAGE -->
+
+<!-- CIRCLE EDGE IMAGE -->
+  <img src="imagesbs/kaniz.jpg" class="img-circle" width="300" height="300"> 
+<!-- CIRCLE EDGE IMAGE -->
+
+<!-- BORDERED IMAGE -->
+  <img src="imagesbs/kaniz.jpg" class="img-thumbnail" width="250" height="250"> 
+<!-- BORDERED IMAGE -->
+
+<!-- RESPONSIVE IMAGE -->
+  <img src="imagesbs/kaniz.jpg" class="img-responsive" width="700" height="700"> 
+<!-- RESPONSIVE IMAGE -->
+  
+</body>
+</html>
