ec76c5dfd1c745c9860ae10ff1cdf2ae562d02be
KCN007 <www.kaniz98@gmail.com>
2020-05-06 02:37:02 +0600

    excercise 1,2,3

---
+++ 3_typography.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Typography</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- The Stylesheet made by BootStrap -->
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+
+<!-- jQuery Library that helps the BootStrap.JS to run -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- the JS file of BootStrap that makes BootStrap Design/Stylesheet Dynamic -->
+<script src="js/bootstrap.min.js"></script>
+
+<style>
+.kaniz {
+	background-color: #769C66;
+	color: #FFFFFF;
+	font-family: Verdana;
+}
+</style>
+
+</head>
+<body>
+
+<!-- HEADING and SECONDARY HEADING -->
+	<h1>H1 Heading <small>Secondary Heading</small></h1>
+	<h2>H2 Heading <small>Secondary Heading</small></h2>
+	<h3>H3 Heading <small>Secondary Heading</small></h3>
+	<h4>H4 Heading <small>Secondary Heading</small></h4>
+	<h5>H5 Heading <small>Secondary Heading</small></h5>
+	<h6>H6 Heading <small>Secondary Heading</small></h6>
+<!-- HEADING and SECONDARY HEADING -->
+
+<!-- 
+	BLOCKQUOTE | Float
+	| class="pull-left" 
+	| class="pull-right"
+-->
+	<blockquote>
+		<p>Glory be to Him Who created all the pairs: from what the earth produces and from themselves and from things unknown to them.</p>
+		<footer>(The Qur'an, 37:36)</footer>
+	</blockquote>
+<!-- BLOCKQUOTE -->
+
+<!-- CODE -->
+	<p>The following HTML Tags: <code>span</code>, <code>section</code>, and <code>div</code> defines a section in a Document.</p>
+<!-- CODE -->
+
+<!-- KEYBOARD INPUT -->
+  <p>Use <kbd>ctrl + a</kbd> to Select Everything.</p>
+<!-- KEYBOARD INPUT -->
+
+<!-- PREPARED STATEMENT -->
+<pre>
+	public static void main()
+	{
+	    printf("Hello World from C Programming Language");
+	}
+</pre>
+<!-- PREPARED STATEMENT -->
+
+<!-- COLORFUL TEXT -->
+	<p class="text-muted">This text is MUTED.</p>
+	<p class="text-primary">This text is PRIMARY.</p>
+	<p class="text-success">This text indicates SUCCESS.</p>
+	<p class="text-info">This text represents some INFORMATION.</p>
+	<p class="text-warning">This text represents a WARNING.</p>
+	<p class="text-danger">This text represents DANGER.</p>
+<!-- COLORFUL TEXT -->
+
+<!-- BACKGROUND COLOR -->
+	<p class="bg-primary">This text is PRIMARY.</p>
+	<p class="bg-success">This text indicates SUCCESS.</p>
+	<p class="bg-info">This text represents some INFORMATION.</p>
+	<p class="bg-warning">This text represents a WARNING.</p>
+	<p class="bg-danger">This text represents DANGER.</p>
+<!-- BACKGROUND COLOR -->
+  
+</body>
+</html>
