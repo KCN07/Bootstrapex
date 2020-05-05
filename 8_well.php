9a6717933e6a7b1b9084c236e310613928d4eb31
KCN007 <www.kaniz98@gmail.com>
2020-05-06 02:43:42 +0600

    excercise 8

---
+++ 8_well.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Case</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body> 
+ 
+<div class="container">
+  <h2>Hello User!</h2>
+  <div class="well well-sm">Your Information is Added Successfully!</div>
+  <div class="well">Sorry Failed!</div>
+  <div class="well well-lg">Thanks for trying!</div>
+</div>
+
+</body>
+</html>
