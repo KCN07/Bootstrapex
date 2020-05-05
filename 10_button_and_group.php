aa3640e96afdabe4d0285e529fc49575e4c40cdd
KCN007 <www.kaniz98@gmail.com>
2020-05-06 03:08:20 +0600

    excercise 10

---
+++ 10_button_and_group.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Buttons</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<style>
+		/* .btn-danger { background-color: #ca110b; } */
+	</style>
+</head>
+<body>
+
+<hr>
+
+<!-- SINGLE BUTTON -->
+	<button type="button" class="btn btn-default">Default</button>
+	<button type="button" class="btn btn-primary">Primary</button>
+	<button type="button" class="btn btn-success">Success</button>
+	<button type="button" class="btn btn-info">Info</button>
+	<button type="button" class="btn btn-warning">Warning</button>
+	<button type="button" class="btn btn-danger">Danger</button>
+	<button type="button" class="btn btn-link">Link</button>
+<!-- SINGLE BUTTON -->
+
+<hr>
+
+<!-- GROUP BUTTON -->
+	<div class="btn-group btn-group-lg">
+		<button type="button" class="btn btn-danger">Preview</button>
+		<button type="button" class="btn btn-danger">Save</button>
+		<button type="button" class="btn btn-danger">Cancel</button>
+	</div>
+
+	<div class="btn-group">
+		<button type="button" class="btn btn-primary">Preview</button>
+		<button type="button" class="btn btn-primary">Save</button>
+		<button type="button" class="btn btn-primary">Cancel</button>
+	</div>
+
+	<div class="btn-group btn-group-xs">
+		<button type="button" class="btn btn-primary">Preview</button>
+		<button type="button" class="btn btn-primary">Save</button>
+		<button type="button" class="btn btn-primary">Cancel</button>
+	</div>
+<!-- GROUP BUTTON -->
+
+<hr>
+
+<!-- BUTTON GROUP JUSTIFIED -->
+	<div class="btn-group btn-group-justified">
+		<a href="#" class="btn btn-primary">Apple</a>
+		<a href="#" class="btn btn-primary">Samsung</a>
+		<a href="#" class="btn btn-primary">Sony</a>
+	</div>
+<!-- BUTTON GROUP JUSTIFIED -->
+	
+</body>
+</html>
