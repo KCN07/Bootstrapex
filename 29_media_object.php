d403d16b0ad51e76a9e242e556c8cb5c27169cdf
KCN007 <www.kaniz98@gmail.com>
2020-05-10 01:25:41 +0600

    excercise 29

---
+++ 29_media_object.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<h2>Media Object</h2>
+
+	<!-- Left-aligned media object -->
+	<div class="media">
+		<div class="media-left">
+			<img src="imagesbs/img_avatar1.png" class="media-object" style="width:60px">
+		</div>
+		<div class="media-body">
+			<h4 class="media-heading">Left-aligned</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+	</div>
+	<hr>
+	
+	<!-- Right-aligned media object -->
+	<div class="media">
+		<div class="media-body">
+		<h4 class="media-heading">Right-aligned</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+		<div class="media-right">
+		<img src="imagesbs/img_avatar1.png" class="media-object" style="width:60px">
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
