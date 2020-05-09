a2810cdaea0a0d55d10fe9c17522cc0193ab0448
KCN007 <www.kaniz98@gmail.com>
2020-05-10 01:05:28 +0600

    excercise 26

---
+++ 26_form_positioning.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- INPUT SIZING -->
+<div class="container">
+	<form role="form">
+		<div class="form-group">
+		
+			<div class="col-md-2">
+				<label for="ex1">First Name</label>
+				<input class="form-control" id="ex1" type="text">
+			</div>
+			
+			<div class="col-md-3">
+				<label for="ex2">Middle Name</label>
+				<input class="form-control" id="ex2" type="text">
+			</div>
+			
+			<div class="col-md-4">
+				<label for="ex3">Last Name</label>
+				<input class="form-control" id="ex3" type="text">
+			</div>
+		
+		</div>
+	</form>
+</div>
+<!-- INPUT SIZING -->
+
+</body>
+</html>
